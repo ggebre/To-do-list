bc8ede1bf60722e1824fcf6f61469546c07eb77b
Getu Gebre <lavumom@yahoo.co.uk>
2019-03-26 22:05:34 -0500

    part 2 complete

--- ToDoList/ToDo.swift
+++ ToDoList/ToDo.swift
@@ -12,4 +12,14 @@ struct ToDo {
     var isComplete: Bool
     var dueDate: Date
     var notes: String?
+    
+    static func loadToDos() -> [ToDo]{
+        return []
+    }
+    static func loadSampleToDos() -> [ToDo]{
+        let todo1 = ToDo(title: "ToDo one", isComplete: false, dueDate: Date(), notes: "Note 1")
+        let todo2 = ToDo(title: "Todo two", isComplete: false, dueDate: Date(), notes: "Note 2")
+        let todo3 = ToDo(title: "ToDo three", isComplete: false, dueDate: Date(), notes: "Note 3")
+        return [todo1, todo2, todo3]
+    }
 }
--- ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoTableViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 class ToDoTableViewController: UITableViewController{
     var todos = [ToDo]()
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if ToDo.loadToDos().count != 0{
+            todos = ToDo.loadToDos()
+        } else {
+            todos = ToDo.loadSampleToDos()
+        }
+    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return todos.count
+        return todos.count 
     }
     override func tableView(_ tableView: UITableView,  cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier") else {
